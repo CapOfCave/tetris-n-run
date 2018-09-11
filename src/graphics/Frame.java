562376fc3ebfe016553938bc1353439ed9e78c6e
Marius1308 <m.schmitz1308@gmx.de>
2018-09-11 21:24:01 +0200

    nix

--- src/graphics/Frame.java
+++ src/graphics/Frame.java
@@ -2,6 +2,7 @@ package graphics;
 
 import javax.swing.JFrame;
 
+
 import graphics.Panel;
 import loading.LevelLoader;
 import logics.GameLoop;
@@ -13,16 +14,22 @@ public class Frame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private Panel panel;
+	
 	private GameLoop gameLoop;
 
 	public static void main(String[] args) {
+
 		
 
 		System.out.println("test marius");
-		new Frame();
-
+		
 	
+
+		new Frame("/res/level.txt");
+
 	}
+
+	//loads a Level
 	public Frame(String url) {
 		panel = new Panel(LevelLoader.loadLevel(url));
 		add(panel);
@@ -35,16 +42,27 @@ public class Frame extends JFrame {
 		gameLoop.start();
 	}
 	
+	//loads the Gui
 	public Frame() {
-		panel = new Panel();
+		//panel = new Panel();
 		add(panel);
-		gameLoop = new GameLoop(panel);
 		setResizable(false);
 		pack();
 		setDefaultCloseOperation(3);
 		setLocationRelativeTo(null);
 		setVisible(true);
-		gameLoop.start();
 	}
+
+//	public Frame() {
+//		panel = new Panel();
+//		add(panel);
+//		gameLoop = new GameLoop(panel);
+//		setResizable(false);
+//		pack();
+//		setDefaultCloseOperation(3);
+//		setLocationRelativeTo(null);
+//		setVisible(true);
+//		gameLoop.start();
+//	}
 
 }
