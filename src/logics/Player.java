8534804f1626ff99d2d76d4cd69d6d9828720ab1
CapOfCave <lars.ke@web.de>
2018-09-13 16:53:02 +0200

    Max speed

--- src/logics/Player.java
+++ src/logics/Player.java
@@ -27,6 +27,7 @@ public class Player {
 	private double vSpeed;
 	private double acc = 0.5;
 	private double brake = 0.8;
+	private double maxSpeed = 3;
 
 	public Player(int blockSize, Camera camera, ArrayList<Tetro> worldTetros, ArrayList<Tetro>[][] worldTetroHitbox, KeyHandler keyHandler) {
 		this.camera = camera;
@@ -108,6 +109,9 @@ public class Player {
 		} else {
 			vSpeed = 0;
 		}
+		
+		hSpeed = Math.min(hSpeed, maxSpeed);
+		vSpeed = Math.min(vSpeed, maxSpeed);
 		
 		x += hSpeed;
 		y += vSpeed;
