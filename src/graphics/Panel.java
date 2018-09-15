4834141fee8cd9d863a1c0d03645f8ac8f4098d2
CapOfCave <lars.ke@web.de>
2018-09-15 23:24:46 +0200

    bugfix#16894

--- src/graphics/Panel.java
+++ src/graphics/Panel.java
@@ -45,7 +45,7 @@ public class Panel extends JPanel implements Playable {
 		tetroTypes = level.getTetroTypes();
 		tetroDrawPositions = new ArrayList<>();
 		for (int i = 0; i < tetroTypes.size(); i++) {
-			tetroDrawPositions.add(new Point(972, i * 100 + 72));
+			tetroDrawPositions.add(new Point(972, i * 85 + 40));
 		}
 
 		inHandHandler = new InHandHandler(tetroTypes, tetroDrawPositions, blockSize, gamePanel, world);
