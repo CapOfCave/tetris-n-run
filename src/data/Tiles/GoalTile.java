0601b9da7cae23c5ff97e9aaeab02df5cc0e7e5d
Marius1308 <m.schmitz1308@gmx.de>
2018-10-20 18:57:52 +0200

    hallo

--- src/data/Tiles/GoalTile.java
+++ src/data/Tiles/GoalTile.java
@@ -1,15 +1,18 @@
 package data.Tiles;
 
 import graphics.Frame;
+import loading.ImageLoader;
 
 public class GoalTile extends Tile {
 
 	public GoalTile(int posX, int posY, Frame frame) {
 		super('!', posX, posY, true, true, frame);
+		img = ImageLoader.loadImage("/res/goal.png");
 	}
 
 	@Override
 	public void eventWhenEntering() {
+		frame.changeToOverworld(false);
 		// TODO Ziel erreicht
 
 	}
