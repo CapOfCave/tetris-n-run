9d86bf46f08f5a42fc0df32b1ce6e2f1d2df7dd4
CapOfCave <lars.kecker2009@gmail.com>
2019-03-28 17:31:14 +0100

    Doch nicht?!

--- src/data/Tiles/SaveNLoadTile.java
+++ src/data/Tiles/SaveNLoadTile.java
@@ -64,9 +64,9 @@ public class SaveNLoadTile extends Tile {
 //					(int) (posY * GameFrame.BLOCKSIZE - world.cameraY()), null);
 //		} TODO remove since not needed
 
-		g.drawImage(akt_animation.getImage(), (int) (posX * GameFrame.BLOCKSIZE - world.cameraX()),
-				(int) (posY * GameFrame.BLOCKSIZE - world.cameraY()), null);
-
+		g.drawImage(akt_animation.getImage(), (int) (posX * GameFrame.BLOCKSIZE - world.cameraX() + akt_animation.getOffsetX()),
+				(int) (posY * GameFrame.BLOCKSIZE - world.cameraY() + akt_animation.getOffsetY()), null);
+		//TODO Tile-Render-Reihenfolge
 	}
 
 	@Override
