c956b6e6e5c78ac741eebbfc2302095221992c0b
CapOfCave <lars.kecker2009@gmail.com>
2019-03-30 23:57:13 +0100

    ventilator (achtung: unschön, needs fix)

--- src/logics/worlds/World.java
+++ src/logics/worlds/World.java
@@ -48,7 +48,7 @@ public abstract class World {
 	protected BufferedImage backgroundImg;
 	protected BufferedImage[] nullTileImgs = { ImageLoader.loadImage("/res/blocks/block0.png"),
 			ImageLoader.loadImage("/res/blocks/block0i.png"), ImageLoader.loadImage("/res/blocks/block0j.png"),
-			ImageLoader.loadImage("/res/blocks/block0k.png"), ImageLoader.loadImage("/res/blocks/block0l.png"), };
+			ImageLoader.loadImage("/res/blocks/ventilator.png"), ImageLoader.loadImage("/res/blocks/block0l.png"), };
 	// protected double[] probs = { 0.53, 0.454, 0.01, 0.005, 0.001 };
 	protected double[] probs = { 1, 0, 0, 0, 0 };
 
